319fad8c9d44d555b7fd21c533f9a3d52ac9a463
unknown <ra120118@uem.br>
2023-12-04 22:46:33 -0300

    IndexPage Traduzida

--- src/pages/en/IndexPage.vue
+++ src/pages/en/IndexPage.vue
@@ -5,39 +5,38 @@
       Welcome!
     </p>
     <p class="text-body1">
-      O objetivo deste portal é auxiliar os professores na utilização/adoção da Aprendizagem Baseada em Exemplos durante o ensino de Engenharia de Software.
+      The purpose of this portal is to assist teachers in the use/adoption of Example-Based Learning during the teaching of Software Engineering.
     </p>
     <p class="text-body1">
-      Você pode usar o nosso <a href="#/tutorial-page">tutorial</a> que vai te ajudar a construir um passo a passo para usar exemplos em sala de aula.
+      You can use our <a href="#/tutorial-page">tutorial</a>, which will help you build a step-by-step guide for incorporating examples in the classroom.
     </p>
     <p class="text-body1">
-      Você também pode navegar pelo menu e ir direto ao tópico desejado caso já tenha um objetivo em mente.
+      You can also navigate through the menu and go directly to the desired topic if you already have a specific goal in mind.
     </p>
     <q-separator />
 
     <p class="text-h5 q-mt-md">
-      Aprendizagem Baseada em Exemplos
+      Example-Based Learning
     </p>
 
     <p class="text-body1">
-      A Aprendizagem Baseada em Exemplos é uma das metodologias ativas que podem ser adotadas pelos professores no ensino de Engenharia de Software.
+      Example-Based Learning is one of the active methodologies that can be adopted by teachers in the teaching of Software Engineering.
     </p>
 
     <p class="text-body1">
-      Segundo Van Gog (2018), essa metodologia de aprendizado se alicerça na demonstração de como realizar determinada tarefa ou habilidade a ser assimilada. Quando um aluno pode observar um exemplo de uma execução bem sucedida de uma tarefa sua crença em ser capaz de realizar o mesmo feito aumenta (HUANG, 2017).
+      According to Van Gog (2018), this learning methodology is based on the demonstration of how to perform a certain task or skill to be assimilated. When a student can observe an example of a successful execution of a task, their belief in being able to perform the same feat increases (HUANG, 2017).
     </p>
     <p class="text-body1">
-      Há várias maneiras de executar as demonstrações nesse processo de aprendizagem. Van Gog e Rummel (2010) citam a utilização de Exemplos Trabalhados (Worked Examples) e Exemplos de Modelagem (Modeling Examples). Com o objetivo de ampliar a visão dos efeitos da utilização de exemplos no processo de aprendizagem, Huang (2017) examinou quatro tipos diferentes de exemplos. Além dos dois já citados, foram examinados Exemplos Trabalhados Errôneos (Erroneous Worked Examples) e Exemplos de Modelagem por Pares (Peer Modeling Examples). A Figura 1 mostra uma classificação dos tipos de exemplos de acordo com a perspectiva pedagógica de cada um deles.
+      There are several ways to perform demonstrations in this learning process. Van Gog and Rummel (2010) mention the use of Worked Examples and Modeling Examples. With the aim of expanding the vision of the effects of the use of examples in the learning process, Huang (2017) examined four different types of examples. In addition to the two already mentioned, Erroneous Worked Examples and Peer Modeling Examples were examined. Figure 1 shows a classification of the types of examples according to the pedagogical perspective of each one.
     </p>
     <q-card class="my-card" style="max-width: 800px">
       <img
         src="img/imagem-geral.png"
-
       >
 
       <q-card-section>
-        <div class="text-h6">Figura 1 - Tipos de exemplos</div>
-        <div class="text-subtitle2">Adaptado de Huang (2017)</div>
+        <div class="text-h6">Figure 1 - Types of examples</div>
+        <div class="text-subtitle2">Adapted from Huang (2017)</div>
       </q-card-section>
 
     </q-card>
@@ -45,7 +44,7 @@
     <q-separator class="q-mt-xl"/>
 
     <p class="text-h5 q-mt-md">
-      Referências
+      References
     </p>
     <p class="text-body2 text-italic">
       HUANG, X. Example-based learning: Effects of different types of examples on student performance, cognitive load and self-efficacy in a statistical learning task. Interactive Learning Environments, Routledge, v. 25, p. 283–294, 4 2017. ISSN 17445191.
@@ -54,7 +53,7 @@
       VAN GOG, T.; RUMMEL, N. Example-based learning: Integrating cognitive and social-cognitive research perspectives. Educational Psychology Review, v. 22, p. 155–174, 2010. ISSN 1040726X.
     </p>
     <p class="text-body2 text-italic">
-      VAN GOG, T. Example-based learning. In: International Handbook of the Learning Sciences. Routledge, 2018. Disponível em: <a href="https://www.routledgehandbooks.com/doi/10.4324/9781315617572-20" target="_blank">https://www.routledgehandbooks.com/doi/10.4324/9781315617572-20</a>
+      VAN GOG, T. Example-based learning. In: International Handbook of the Learning Sciences. Routledge, 2018. Available at: <a href="https://www.routledgehandbooks.com/doi/10.4324/9781315617572-20" target="_blank">https://www.routledgehandbooks.com/doi/10.4324/9781315617572-20</a>
     </p>
 
   </q-page>
@@ -65,6 +64,5 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'IndexPage'
-
 })
 </script>
