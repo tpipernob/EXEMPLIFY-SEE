82868ab142374d40fe88221c83f5f1834585f067
unknown <ra120118@uem.br>
2023-12-14 01:23:40 -0300

    Versão em Inglês completa

--- src/pages/en/TeachingTheoryPage.vue
+++ src/pages/en/TeachingTheoryPage.vue
@@ -2,9 +2,9 @@
   <q-page padding>
 
     <p class="text-h5">
-      Ensino da teoria
+      Teaching Theory
     </p>
-    <p>Ao clicar na seta os passos sugeridos são exibidos.</p>
+    <p>Clicking on the arrow displays the suggested steps.</p>
 
     <div>
     <q-splitter
@@ -32,28 +32,28 @@
           transition-prev="jump-up"
           transition-next="jump-up"
         >
-          <q-tab-panel name="Aula Expositiva">
-            <div class="text-h4 q-mb-md">Aula Expositiva</div>
-            <p>Essa etapa é sugerida para casos em que os alunos ainda não tenham tipo contato com o conteúdo.</p>
-            <p>Esse tipo de abordagem se mostrou efetiva quando utilizada como ponto inicial do processo de ensino</p>
-            <p>A última etapa do processo consiste na utilização de exemplos para ajudar os alunos a relacionar o conteúdo com uma situação concreta.</p>
+          <q-tab-panel name="Expository Lecture">
+            <div class="text-h4 q-mb-md">Expository Lecture</div>
+            <p>This step is suggested for cases in which students have not yet had contact with the content.</p>
+            <p>This approach has proven effective when used as the initial point in the teaching process.</p>
+            <p>The final stage of the process involves using examples to help students relate the content to a concrete situation.</p>
           </q-tab-panel>
 
-          <q-tab-panel name="Sala de aula invertida">
-            <div class="text-h4 q-mb-md">Sala de aula invertida</div>
-            <p>Essa etapa é sugerida para casos em que os alunos ainda não tenham tipo contato com o conteúdo.</p>
-            <p>A utilização da Sala de Aula Invertida consiste em transpor a exposição do conteúdo para antes do período da aula por meio de vídeos, artigos ou livros. Isso contribui para tornar a absorção de conteúdo mais eficaz e envolvente.</p>
-            <p>A última etapa do processo consiste na utilização de exemplos para ajudar os alunos a relacionar o conteúdo com uma situação concreta.</p>
-            <p>Para mais informações a respeito deste método de ensino, acessar as referências a seguir:</p>
+          <q-tab-panel name="Flipped Classroom">
+            <div class="text-h4 q-mb-md">Flipped Classroom</div>
+            <p>This step is suggested for cases in which students have not yet had contact with the content.</p>
+            <p>The use of the Flipped Classroom involves moving the content delivery to before the class period through videos, articles, or books. This contributes to making the absorption of content more effective and engaging.</p>
+            <p>The final stage of the process involves using examples to help students relate the content to a concrete situation.</p>
+            <p>For more information about this teaching method, refer to the following references:</p>
             <ul>
               <li><a href="https://www.jite.org/documents/Vol17/JITEv17IIPp181-200Joy4734.pdf" target="_blank" rel="noopener noreferrer">Activity Oriented Teaching Strategy for Software Engineering Course: An Experience Report</a></li>
               <li><a href="https://omerad.msu.edu/teaching/teaching-skills-strategies/27-teaching/162-what-why-and-how-to-implement-a-flipped-classroom-model" target="_blank" rel="noopener noreferrer">What, why, and how to implement a flipped classroom model</a></li>
             </ul>
           </q-tab-panel>
 
-          <q-tab-panel name="Outras metodologias">
-            <div class="text-h4 q-mb-md">Outras metodologias</div>
-            <p>Em construção.</p>
+          <q-tab-panel name="Other methodologies">
+            <div class="text-h4 q-mb-md">Other methodologies</div>
+            <p>Under construction.</p>
           </q-tab-panel>
 
         </q-tab-panels>
@@ -72,42 +72,42 @@ export default defineComponent({
   setup () {
     return {
       splitterModel: ref(30),
-      selected: ref('Aula Expositiva'),
+      selected: ref('Expository Lecture'),
 
       simple: [
         {
-          label: 'Aula Expositiva',
+          label: 'Expository Lecture',
           children: [
             {
-              label: '- Palestra',
+              label: '- Lecture',
               disabled: true
             },
             {
-              label: '- Explicar exemplos dos conceitos ensinados',
+              label: '- Explain examples of the taught concepts.',
               disabled: true
             }
           ]
         },
         {
-          label: 'Sala de aula invertida',
+          label: 'Flipped Classroom',
           children: [
             {
-              label: '- Compartilhar o material',
+              label: '- Share the material',
               disabled: true
             },
             {
-              label: '- Revisão dos conceitos e tira dúvidas',
+              label: '- Review of concepts and Q&A (Questions and Answers).',
               disabled: true
             },
             {
-              label: '- Explicar exemplos dos conceitos ensinados',
+              label: '- Explain examples of the taught concepts.',
               disabled: true
 
             }
           ]
         },
         {
-          label: 'Outras metodologias',
+          label: 'Other methodologies',
           children: [
             {
               label: '',
